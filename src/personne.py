ef70e5fc3ae0d196cf94092eb3a44a0ffd2b2341
id3037MB <meline.bonavero@eleve.ensai.fr>
2026-03-11 14:13:37 +0000

    Modifications de la classe Personne

--- src/personne.py
+++ src/personne.py
@@ -6,29 +6,33 @@ class Personne:
 
         if not isinstance(nom, str):
             raise TypeError("Le nom doit être une chaîne de caractères")
-        self.nom = nom 
+        self.nom = nom
 
         if not isinstance(age, int):
             raise TypeError("L'âge doit être un nombre entier")
         if self.age < 0:
             raise ValueError("L'âge doit être un entier positif")
-        self.age = age 
+        self.age = age
 
         if not isinstance(taille, float):
             raise TypeError("La taille doit être un réel")
         if self.taille < 0:
             raise ValueError("La taille doit être un réel positif")
-        self.taille = taille 
+        self.taille = taille
 
     def __str__(self) -> str:
         return f"{self.prenom} {self.nom} a {self.age} ans et mesure {self.taille} m"
 
-    def conversion(self) -> float: 
+    def conversion(self) -> float:
         '''
         La fonction permet de convertir la taille en cm en m.
         '''
         return self.taille * 0.01
-    
-    def __repr__ (self) -> str:
-        return f"Personne(prenom: {self.prenom}, nom: {self.nom}, age: {self.age}, taille: {self.taille})"
 
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(prenom: {self.prenom},",
+            f"nom: {self.nom},",
+            f"age: {self.age},",
+            f"taille: {self.taille})"
+            )
